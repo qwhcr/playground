a60de81bd5bc0bf1d907b86d9bdf210051c2f8ec
QiusenHuangGT <qiusenhuang17@hotmail.com>
2019-02-28 21:59:09 -0500

    add html and css

--- node/app.js
+++ node/app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path')
+
 app.get('/', function(req,res) {
-	res.send('Hello World!');
+	res.sendFile(path.join(__dirname + '/html/Untitled-1.html'))
+});
+
+app.get('/style.css', function(req,res) {
+	res.sendFile(path.join(__dirname + '/html/style.css'))
 });
 
 app.listen(3000, function() {
